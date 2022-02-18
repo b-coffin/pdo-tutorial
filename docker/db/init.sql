773a7c4e6c9aa746a408bb8807fd749243ca3b03
Kyoya Takei <takei.kyoya.su@alumni.tsukuba.ac.jp>
2022-02-18 23:05:41 +0900

    docker起動時にDBにテーブルと新規行を追加する

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE homestead;
+CREATE DATABASE IF NOT EXISTS homestead;
+
+CREATE TABLE IF NOT EXISTS homestead.users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE InnoDB DEFAULT CHARACTER SET utf8;
+
+INSERT INTO homestead.users SET name='Taro Yamada';
