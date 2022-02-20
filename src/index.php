029dfcc9a717f4bc55cf37829d82eed4b2eb74be
Kyoya Takei <takei.kyoya.su@alumni.tsukuba.ac.jp>
2022-02-20 15:18:47 +0900

    クエリの実行結果をテーブルとして表示

--- src/index.php
+++ src/index.php
@@ -2,6 +2,19 @@
     <head>
         <meta charset="utf-8">
         <title>PDO Tutorial</title>
+        <style>
+            table {
+                border-collapse: collapse;
+            }
+
+            td, th {
+                padding: 0.5em;
+            }
+
+            table, td, th {
+                border: solid;
+            }
+        </style>
     </head>
     <body>
         <h3>PDO Tutorial</h3>
@@ -10,11 +23,11 @@
                 $pdo = new PDO('mysql:host=db;dbname=homestead;port=3306', 'root', 'secret');
                 $result = $pdo->query('SELECT * FROM users');
                 if ($result instanceof PDOStatement) {
+                    print('<table><thead><tr><th>id</th><th>name</th></tr></thead><tbody>');
                     foreach ($result as $row) {
-                        print_r($row);
+                        print('<tr><td>'.$row['id'].'</td><td>'.$row['name'].'</td></tr>');
                     }
-                } else {
-                    print_r($result);
+                    print('</tbody></table>');
                 }
             } catch (PDOException $e) {
                 echo $e->getMessage();
